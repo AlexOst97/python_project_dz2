3a5455260a8ba2fb6aca548b803f68357e550c9d
unknown <sanya@example.com>
2024-11-01 22:00:52 +0700

    All

--- src/masks.py
+++ src/masks.py
@@ -8,5 +8,5 @@ def get_mask_account(account_number: str) -> str:
     return "**" + account_number[16:]
 
 
-print(get_mask_card_number("7000792289606361"))
-print(get_mask_account("73654108430135874305"))
+#print(get_mask_card_number("7000792289606361"))
+#print(get_mask_account("73654108430135874305"))
