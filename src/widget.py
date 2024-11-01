a8066bc16dfe95f0ea5754e9af7f40e25a92bfba
unknown <sanya@example.com>
2024-11-01 20:09:23 +0700

    widget.py_2

--- src/widget.py
+++ src/widget.py
@@ -1,10 +1,21 @@
-from masks import get_mask_card_number
 from masks import get_mask_account
+from masks import get_mask_card_number
 
 def mask_account_card(bank_account: str) -> str:
-    """ Функция, которая обрабатывает информацию о картах и счетах."""
+    """Функция, которая обрабатывает информацию о картах и счетах"""
     if 'Счет' in bank_account:
         bank_account_account = get_mask_account(bank_account[-20:])
         return (f'Счет {bank_account_account}')
+    else:
+        bank_account_card = get_mask_card_number(bank_account[-16:])
+        return (f'{bank_account[0:-17]} {bank_account_card}')
 
+
+print(mask_account_card('Maestro 1596837868705199'))
+print(mask_account_card('Счет 64686473678894779589'))
+print(mask_account_card('MasterCard 7158300734726758'))
+print(mask_account_card('Счет 35383033474447895560'))
+print(mask_account_card('Visa Classic 6831982476737658'))
+print(mask_account_card('Visa Platinum 8990922113665229'))
+print(mask_account_card('Visa Gold 5999414228426353'))
 print(mask_account_card('Счет 73654108430135874305'))
